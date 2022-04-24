cece24c3a08ee62e328678e6bcb1d483342486bf
kDutchie <76678787+kDutchie@users.noreply.github.com>
2022-04-24 23:32:51 +0530

    Adding pracs 2-8

---
+++ Practical 5/websocketserver/src/java/source/newclass1.java
@@ -0,0 +1,37 @@
+package source;
+import java.io.IOException;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+@ServerEndpoint("/echo")
+public class newclass1 {
+    @OnOpen
+    public void onOpen(Session session) {
+        System.out.println("onOpen::" + session.getId());        
+    }
+    @OnClose
+    public void onClose(Session session) {
+        System.out.println("onClose::" +  session.getId());
+    }
+    
+    @OnMessage
+    public void onMessage(String message, Session session) {
+        System.out.println("onMessage::From=" + session.getId());
+        
+        try {
+            session.getBasicRemote().sendText(message.toUpperCase());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    @OnError
+    public void onError(Throwable t) {
+        System.out.println("onError::" + t.getMessage());
+    }
+    public static void main(String args[]){}
+            
+}
